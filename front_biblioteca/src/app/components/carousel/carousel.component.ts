5c73369df9a7b0c0e1cbfff8a224fdddc8fdaeb1
typicalmoods <ananicol23@gmail.ccom>
2023-04-22 18:14:08 +0200

    add basic features for front

---
+++ front_biblioteca/src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent {
+
+}
